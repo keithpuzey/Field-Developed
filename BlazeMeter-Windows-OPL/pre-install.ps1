41302f6dda5144637720e7b909639acdb8ca0854
keithpuzey <keithpuzey@users.noreply.github.com>
2020-11-09 17:10:31 +0000

    Fixed Script

--- BlazeMeter-Windows-OPL/pre-install.ps1
+++ BlazeMeter-Windows-OPL/pre-install.ps1
@@ -4,7 +4,7 @@ Clear-Host
 #
 $ComputerName=$env:COMPUTERNAME
 $IEAdminRegistryKey="HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$IEUserRegistryKey ="HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+$IEUserRegistryKey ="HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
 
 [string[]]$authurls = ( 
     "https://bard.blazemeter.com",
