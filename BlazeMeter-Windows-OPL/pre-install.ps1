6406b00ee1757b7f14e57a9f4c2eb07fbc098ca9
keithpuzey <keithpuzey@users.noreply.github.com>
2020-11-16 14:46:00 +0000

    Removed message on windows 10
    IE Enhanced Security message added for Windows 10

--- BlazeMeter-Windows-OPL/pre-install.ps1
+++ BlazeMeter-Windows-OPL/pre-install.ps1
@@ -125,6 +125,10 @@ if ((Get-ItemProperty $zoomfactorpath -Name $zoomfactorkey -EA 0).$zoomfactorkey
 }
 
 
+if ($os -like "Microsoft Windows 10*") {
+Write-Host "`nInternet Explorer Enhanced Security not available on this platform $os"   -ForegroundColor Black -BackgroundColor Green }
+else
+{ 
 if ((Test-Path -Path $IEAdminRegistryKey) -or (Test-Path -Path $IEUserRegistryKey)) {
     $IEAdminRegistryValue=(Get-ItemProperty -Path $IEAdminRegistryKey -Name IsInstalled).IsInstalled
     if ($IEAdminRegistryKey -ne "" ) {
@@ -146,7 +150,7 @@ if ((Test-Path -Path $IEAdminRegistryKey) -or (Test-Path -Path $IEUserRegistryKe
 } else {
 Write-Host "`n" 
 }
-
+}
 
     # Write-Host "IE Protected Mode Configuration"  -ForegroundColor Black -BackgroundColor Green
 	
@@ -161,7 +165,7 @@ function changeIeProtectedMode{
                                                                     else{'n/a'}                                                                                        
                                                                     }}
     if ($keys.status -contains 'disabled')
-		{ write-host "IE Zones Protected mode not enabled on all zones " -ForegroundColor Black -BackgroundColor Red 
+		{ write-host "`nIE Zones Protected mode not enabled on all zones " -ForegroundColor Black -BackgroundColor Red 
         write-host "`n$($keys|out-string)" # Key codes 0: Enabled, 3: Disabled 
 	}
 	
@@ -195,12 +199,12 @@ function changeIeProtectedMode{
     # else{write-host 'No changes have been made'}
     
  
-    else { write-host "IE Zones Protected mode configured correctly" -ForegroundColor Black -BackgroundColor Green  }}
+    else { write-host "`nIE Zones Protected mode configured correctly" -ForegroundColor Black -BackgroundColor Green  }}
 	
 changeIeProtectedMode
 
 
-write-host "`n Required Applications " -ForegroundColor White -BackgroundColor Black
+write-host "`nRequired Applications " -ForegroundColor White -BackgroundColor Black
 
 
 $registryExists = Test-Path HKLM:\Software\Wow6432Node\python
@@ -208,11 +212,12 @@ $registryExists = Test-Path HKLM:\Software\Wow6432Node\python
 if ($registryExists -eq $True ) {
 
 	$python = & python -V 2>&1
-    Write-Host "$python is installed"  -ForegroundColor Black -BackgroundColor Green
-    $pipbzt = & python -m pip freeze
+    Write-Host "`n$python is installed"  -ForegroundColor Black -BackgroundColor Green
+    $pipbzt = & python -m pip freeze | findstr boto3 
+    # $pipbzt = & python -m pip list | findstr boto3 
 
     if($pipbzt -eq $null){
-    Write-Host " Python Blazemeter Packages not installed" -ForegroundColor Black -BackgroundColor Red
+    Write-Host " `nPython Blazemeter Packages not installed" -ForegroundColor Black -BackgroundColor Red
            
     }
     Else {
@@ -225,34 +230,34 @@ if ($registryExists -eq $True ) {
  
 
     if($botocoreversion -like "WARNING"){
-    Write-Host "boto core not installed" -ForegroundColor Black -BackgroundColor Red
+    Write-Host "`nboto core not installed" -ForegroundColor Black -BackgroundColor Red
            
     }
-    Else {Write-Host "Boto Core  $botocoreversion installed " -ForegroundColor Black -BackgroundColor Green }
+    Else {Write-Host "`nBoto Core  $botocoreversion installed " -ForegroundColor Black -BackgroundColor Green }
 
     if($boto3version -like "WARNING"){
-    Write-Host "boto3 not installed" -ForegroundColor Black -BackgroundColor Red
+    Write-Host "`nboto3 not installed" -ForegroundColor Black -BackgroundColor Red
            
     }
-    Else {Write-Host "Boto3 $boto3version installed" -ForegroundColor Black -BackgroundColor Green }
+    Else {Write-Host "`nBoto3 $boto3version installed" -ForegroundColor Black -BackgroundColor Green }
     if($bzmcommon -like "WARNING"){
-    Write-Host "bzmcommon not installed" -ForegroundColor Black -BackgroundColor Red
+    Write-Host "`nbzmcommon not installed" -ForegroundColor Black -BackgroundColor Red
            
     }
-    Else {Write-Host "bzmcommon $bzmcommon installed" -ForegroundColor Black -BackgroundColor Green }
+    Else {Write-Host "`nbzmcommon $bzmcommon installed" -ForegroundColor Black -BackgroundColor Green }
     }
     }
- Else {Write-Host "Python is not installed"  -ForegroundColor Black -BackgroundColor Red}
+ Else {Write-Host "`nPython is not installed"  -ForegroundColor Black -BackgroundColor Red}
 
     $MSVSC = Get-WmiObject -Class Win32_Product | sort-object Name | Where-Object { $_.name -like "*Microsoft Visual C++ *Additional Runtime*"}
     if($MSVSC -eq $null){
-    Write-Host " Microsoft Visual C++ Additional Runtime is not installed" -ForegroundColor Black -BackgroundColor Red
+    Write-Host "`nMicrosoft Visual C++ Additional Runtime is not installed" -ForegroundColor Black -BackgroundColor Red
            
     }
-    Else {Write-Host " Microsoft Visual C++ Additional Runtime is installed" -ForegroundColor Black -BackgroundColor Green }
+    Else {Write-Host "`nMicrosoft Visual C++ Additional Runtime is installed" -ForegroundColor Black -BackgroundColor Green }
 
 
-write-host "`n Network Connectivity " -ForegroundColor White -BackgroundColor Black
+write-host "`nNetwork Connectivity " -ForegroundColor White -BackgroundColor Black
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
@@ -269,11 +274,11 @@ try  {
     
 
     if ($StatusCode -eq "200") {
-write-host "$url is up (Return code: $StatusCode ) $line " -ForegroundColor green 
+write-host "`n$url is up (Return code: $StatusCode ) $line " -ForegroundColor green 
 }
 
 else {
-write-host "$url is down `n" ` -ForegroundColor red
+write-host "`n$url is down `n" ` -ForegroundColor red
 }
 
 
@@ -283,7 +288,7 @@ write-host "$url is down `n" ` -ForegroundColor red
 catch
 {
     $StatusCode = $_.Exception.Response.StatusCode.value__
-    write-host "`n $url is down `n" ` -ForegroundColor red
+    write-host "`n$url is down `n" ` -ForegroundColor red
 }
 
 }
@@ -297,27 +302,27 @@ foreach($authurl in $authurls)
       $checkConnection = Invoke-WebRequest -Uri $authurl
       Write-Host $checkConnection
       if ($checkConnection.StatusCode -eq 200) {
-         Write-Host "$authurl  Connection Verified!" -ForegroundColor Green
+         Write-Host "`n$authurl  Connection Verified!" -ForegroundColor Green
 
       }
       else {
-      write-host "$authurl  Connection Failed`n" ` -ForegroundColor red
+      write-host "`n$authurl  Connection Failed`n" ` -ForegroundColor red
      } 
    }
 
    catch [System.Net.WebException] {
       $exceptionMessage = $Error[0].Exception
       if ($exceptionMessage -match "503") {
-         Write-Host "$authurl  Server Unavaiable" -ForegroundColor Red
+         Write-Host "`n$authurl  Server Unavaiable" -ForegroundColor Red
       }
       elseif ($exceptionMessage -match "404") {
-         Write-Host "$authurl  Error 404" -ForegroundColor Red
+         Write-Host "`n$authurl  Error 404" -ForegroundColor Red
       }
       elseif ($exceptionMessage -match "401") {
-         write-host "$authurl is up (Return code: 401 ) $line " -ForegroundColor green 
+         write-host "`n$authurl is up (Return code: 401 ) $line " -ForegroundColor green 
                }
       elseif ($exceptionMessage -match "400") {
-      write-host "$authurl is up (Return code: 400 ) $line " -ForegroundColor green 
+      write-host "`n$authurl is up (Return code: 400 ) $line " -ForegroundColor green 
       }
       
    }
