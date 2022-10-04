9343ce981be07d9448b34a92b32d49ff5673944b
keithpuzey <keithpuzey@users.noreply.github.com>
2022-10-04 10:52:07 +0100

    CSV Upload

--- demowebsite/finance-original/upload.php
+++ demowebsite/finance-original/upload.php
@@ -1,49 +1,32 @@
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$csv = array();
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
+// check there are no errors
+if($_FILES['csv']['error'] == 0){
+    $name = $_FILES['csv']['name'];
+    $ext = strtolower(end(explode('.', $_FILES['csv']['name'])));
+    $type = $_FILES['csv']['type'];
+    $tmpName = $_FILES['csv']['tmp_name'];
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
+    // check the file is a csv
+    if($ext === 'csv'){
+        if(($handle = fopen($tmpName, 'r')) !== FALSE) {
+            // necessary if a large csv file
+            set_time_limit(0);
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 50000000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
+            $row = 0;
 
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
+            while(($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
+                // number of fields in the csv
+                $col_count = count($data);
 
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
+                // get the values from the csv
+                $csv[$row]['col1'] = $data[0];
+                $csv[$row]['col2'] = $data[1];
+
+                // inc the row
+                $row++;
+            }
+            fclose($handle);
+        }
+    }
 }
-?>
